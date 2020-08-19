325741cd34f58d6a89b196a6a7b956f3179516fa
Cristina Altreche <c.altreche@gmail.com>
2020-08-19 16:35:01 -0400

    repair testing complete

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -26,4 +26,28 @@ describe("repair()", function () {
       const actual = enhancer.repair(item)
       expect(actual).toStrictEqual(expected)
   });
+
+  it("should return a message you item has max durability", function() {
+      const item = {
+          weapon: "Mace",
+          durability: 100,
+          enhancement: 15
+      }
+
+      const expected = { message: "your item already has max durability"}
+      const actual = enhancer.repair(item)
+      expect(actual).toStrictEqual(expected)
+  })
+
+  it('should return a message if durability is less than 0', function() {
+      const item = {
+          weapon: "polearm",
+          durability: -5,
+          enhancement: 15
+      }
+
+      const expected = { message: "your item is broken"}
+      const actual = enhancer.repair(item)
+      expect(actual).toStrictEqual(expected)
+  })
 });
